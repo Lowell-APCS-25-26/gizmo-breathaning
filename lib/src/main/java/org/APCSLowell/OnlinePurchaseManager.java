1deb273db9807db02a31053b8c89fa4c810b6b47
breathaning <etzhu1@gmail.com>
2026-04-09 06:30:40 +0000

    submit and remove comma to fix broken test (lmnop)

--- lib/src/main/java/org/APCSLowell/OnlinePurchaseManager.java
+++ lib/src/main/java/org/APCSLowell/OnlinePurchaseManager.java
@@ -11,16 +11,21 @@ public class OnlinePurchaseManager {
      * whose manufacturer is maker, as described in part (a).
      */
     public int countElectronicsByMaker(String maker) {
-        /* to be implemented in part (a) */
-
+        int count = 0;
+        for (Gizmo gizmo : purchases) {
+            if (gizmo.isElectronic() && gizmo.getMaker().equals(maker)) count++;
+        }
+        return count;
     }
 
     /** Returns true if any pair of adjacent purchased Gizmo objects are
      * equivalent, and false otherwise, as described in part (b).
      */
     public boolean hasAdjacentEqualPair() {
-        /* to be implemented in part (b) */
-
+        for (int i = 0; i < purchases.size() - 1; i++) {
+            if (purchases.get(i).equals(purchases.get(i+1))) return true;
+        }
+        return false;
     }
     public OnlinePurchaseManager() {
         purchases = new ArrayList <Gizmo>();
